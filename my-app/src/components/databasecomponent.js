1762b3bb9dcc259bc61dacb3f2ca8450cbc1ed12
Damien Cunningham <damienryan@me.com>
2024-01-06 16:16:28 -0500

    Delete my-app directory

--- my-app/src/components/databasecomponent.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Cru:8NPXXAVN2DSfsAHa@cluster0.al2xj16.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-const collection = client.db("Rey").collection("transcripts");
-// perform actions on the collection object
-client.close();
-});
--- my-app/src/main.js
+++
@@ -1,3 +0,0 @@
-const analyzeData = require('./data-analysis.js').analyzeData;
-const result = analyzeData();
-console.log(result);
