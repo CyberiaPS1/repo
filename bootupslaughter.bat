2b8a749a008de18f7a84d4542c57eb4390032f05
Damien Cunningham <damienryan@me.com>
2024-08-13 12:48:36 -0400

    Delete bootupslaughter.bat

--- bootupslaughter.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Replace 'ServiceName' with the actual service names you want to disable
-sc stop GamingServices
-sc config GamingServices start= disabled
-sc stop ServiceName2
-sc config ServiceName2 start= disabled
-:: Add more services as needed
